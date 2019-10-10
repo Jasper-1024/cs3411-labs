9936f688b086d8d8b7cb933494e5ec8e768d1d62
Brian Morgan <brianmorgan2013@gmail.com>
2019-10-10 15:31:19 -0400

    Implement ex1, copy to ex2

--- lab7/ex1.c
+++ lab7/ex1.c
@@ -1,19 +1,42 @@
 #include "circle.h"
-
+#include <pthread.h>
+#define NUM_THREADS 1
 /* shared variable for the threads to update */
 volatile int var = -1;
+pthread_mutex_t lock;
+
+void *
+wrapper(__attribute__((unused)) void *v){
+    pthread_mutex_lock(&lock);
+    if(var == -1){
+        var = circle();
+    }
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
 
 int
 main(void)
 {
 	/* number of samples */
-	int niters = 10000000;
+	int niters = 10000;
 	int count = 0;
 	int i = 0;
+    pthread_t pid;
+    //pthread_create(&pid, NULL, &wrapper, NULL); 
+	for(i = 0; i < niters; i++) {
+        pthread_create(&pid, NULL, &wrapper, NULL); 
 
-	for (; i < niters; i++) {
-		if (circle() == 1)
-			count++;
+        if(var == 0 || var == 1){
+            if(var == 1){
+                count++;
+            }
+            var = -1;
+        }
+        pthread_join(pid, NULL);
+    //    if (circle() == 1)
+	//		count++;
 	}
 
 	printf("The value of pi is: %f\n", (double)count / niters * 4);
--- lab7/ex2.c
+++ lab7/ex2.c
@@ -1,22 +1,43 @@
 #include "circle.h"
-
+#include <pthread.h>
+#define NUM_THREADS 1
 /* shared variable for the threads to update */
 volatile int var = -1;
+pthread_mutex_t lock;
+
+void *
+wrapper(__attribute__((unused)) void *v){
+    pthread_mutex_lock(&lock);
+    if(var == -1){
+        var = circle();
+    }
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
 
 int
 main(void)
 {
-        /* number of samples */
-        int niters = 10000000;
-        int count = 0;
-        int i = 0;
+	/* number of samples */
+	int niters = 10000;
+	int count = 0;
+	int i = 0;
+    pthread_t pid;
+    //pthread_create(&pid, NULL, &wrapper, NULL); 
+	for(i = 0; i < niters; i++) {
+        pthread_create(&pid, NULL, &wrapper, NULL); 
 
-        for (; i < niters; i++) {
-                if (circle() == 1)
-                        count++;
+        if(var == 0 || var == 1){
+            if(var == 1){
+                count++;
+            }
+            var = -1;
         }
+        pthread_join(pid, NULL);
+	}
 
-        printf("The value of pi is: %f\n", (double)count / niters * 4);
+	printf("The value of pi is: %f\n", (double)count / niters * 4);
 
-        return 0;
+	return 0;
 }
